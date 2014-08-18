94f58bd14b0818a0b9d34da6f2991e047a51db9f
shravya <intonedevelop@192.168.2.43>
2014-08-18 08:38:39 -0400

    commit

--- myproject/src/com/example/myproject/MainActivity.java
+++ myproject/src/com/example/myproject/MainActivity.java
@@ -4,12 +4,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.button1);
+        btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplication(), "clicked", Toast.LENGTH_LONG).show();
+			}
+		});
     }
 }
